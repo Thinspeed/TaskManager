09ddcd74a4ad29be41480fb7831488de45108dc6
Thinspeed <offmailno5@gmail.com>
2025-04-04 20:27:42 +0300

    Add cors definition

---
+++ TaskManager.Api/AppDefinitions/CorsDefinition.cs
@@ -0,0 +1,32 @@
+using AppDefinition.Abstractions;
+
+namespace TaskManager.Api.AppDefinitions;
+
+public class CorsDefinition : IAppDefinition
+{
+    public const string CorsPolicyName = "corsDefaultPolicy";
+    
+    public void RegisterDefinition(IHostApplicationBuilder builder)
+    {
+        string allowedHosts = builder.Configuration.GetSection("CorsOrigins").Value ?? string.Empty;
+        string[] origins = allowedHosts.Split(" ");
+        
+        builder.Services.AddCors(options => options.AddPolicy(name: CorsPolicyName, policy =>
+        {
+            policy.WithOrigins(origins)
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowCredentials();
+        }));
+    }
+
+    public void Init(IHost app)
+    {
+        if (app is not IApplicationBuilder appBuilder)
+        {
+            throw new Exception("App definitions must implement IApplicationBuilder interface to add Cors middleware.");
+        }
+        
+        appBuilder.UseCors(CorsPolicyName);
+    }
+}
