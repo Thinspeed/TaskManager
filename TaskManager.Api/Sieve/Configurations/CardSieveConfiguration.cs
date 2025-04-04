ba24192c74add6380ec96cccd5096c524f1ce9a6
Thinspeed <offmailno5@gmail.com>
2025-04-04 09:48:01 +0300

    Add sieve configuration

---
+++ TaskManager.Api/Sieve/Configurations/CardSieveConfiguration.cs
@@ -0,0 +1,28 @@
+using JetBrains.Annotations;
+using Sieve.Services;
+using TaskManager.Domain;
+
+namespace TaskManager.Api.Sieve.Configurations;
+
+[UsedImplicitly]
+public class CardSieveConfiguration : ISieveConfiguration
+{
+    public void Configure(SievePropertyMapper mapper)
+    {
+        mapper.Property<Card>(x => x.CreationDate)
+            .CanSort()
+            .CanFilter();
+
+        mapper.Property<Card>(x => x.ClosingDate)
+            .CanSort()
+            .CanFilter();
+        
+        mapper.Property<Card>(x => x.User.Id)
+            .CanSort()
+            .CanFilter();
+        
+        mapper.Property<Card>(x => x.Status)
+            .CanSort()
+            .CanFilter();
+    }
+}
