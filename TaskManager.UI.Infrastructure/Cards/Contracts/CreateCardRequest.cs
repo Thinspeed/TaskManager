3d2a2ce9b3a616031c4debad881b6b53b8a50f9a
Thinspeed <offmailno5@gmail.com>
2025-04-05 21:11:59 +0300

    Add create card modal

--- TaskManager.UI.Infrastructure/Cards/Contracts/CreateCardRequest.cs
+++ TaskManager.UI.Infrastructure/Cards/Contracts/CreateCardRequest.cs
@@ -1,10 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+using TaskManager.UI.Infrastructure.Shared;
+
 namespace TaskManager.UI.Infrastructure.Cards.Contracts;
 
 public class CreateCardRequest
 {
+    [Required(ErrorMessage = "Название обязательно")]
     public string Name { get; set; }
     
     public string Description { get; set; }
     
-    DateTime EstimatedCompletionDate { get; set; }
+    [Required(ErrorMessage = "Дата обязательна")]
+    [FutureDate(ErrorMessage = "Дата должна быть в будущем")]
+    public DateTime EstimatedCompletionDate { get; set; }
 }
