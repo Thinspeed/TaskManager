fb15862673bebe62fd2b0ee21f0f17b1ea2b4e58
Thinspeed <offmailno5@gmail.com>
2025-04-05 15:03:41 +0300

    Add UI project

---
+++ TaskManager.UI/Program.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using TaskManager.UI;
+using TaskManager.UI.Infrastructure.Auth;
+using TaskManager.UI.Infrastructure.User;
+using TaskManager.UI.Services;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped(sp =>
+{
+    var client = new HttpClient
+    {
+        BaseAddress = new Uri("https://localhost:8081/")
+    };
+    
+    return client;
+});
+
+builder.Services.AddScoped<AuthorizedHttpClient>();
+builder.Services.AddScoped<CustomAuthStateProvider>();
+builder.Services.AddScoped<AuthenticationStateProvider>(sp => 
+    sp.GetRequiredService<CustomAuthStateProvider>());
+builder.Services.AddAuthorizationCore(); 
+
+
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
