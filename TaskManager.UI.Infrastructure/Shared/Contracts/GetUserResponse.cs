a35b465310ba06bf5fc3671706a2deeb96924d21
Thinspeed <offmailno5@gmail.com>
2025-04-05 19:18:03 +0300

    Fix id type

--- TaskManager.UI.Infrastructure/Shared/Contracts/GetUserResponse.cs
+++ TaskManager.UI.Infrastructure/Shared/Contracts/GetUserResponse.cs
@@ -2,7 +2,7 @@ namespace TaskManager.UI.Infrastructure.Shared.Contracts;
 
 public class GetUserResponse
 {
-    public string Id { get; set; }
+    public int Id { get; set; }
     
     public string Name { get; set; }
 }
